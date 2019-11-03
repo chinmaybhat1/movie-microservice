106d0d4c4d98b26e6ffd70d76fef0ee61159d749
Chinmay Bhat <chinmay.bhat@cerner.com>
2019-11-03 11:39:33 +0530

    Using WebClioent to make API calls

--- movie-catelog-service/src/main/java/com/microservice/moviecatelogservice/resources/MovieCatelogResource.java
+++ movie-catelog-service/src/main/java/com/microservice/moviecatelogservice/resources/MovieCatelogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.microservice.moviecatelogservice.models.CatelogItem;
 import com.microservice.moviecatelogservice.models.Movie;
@@ -21,13 +22,21 @@ public class MovieCatelogResource {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+
 	@RequestMapping("/{userId}")
 	public List<CatelogItem> getCatelog(@PathVariable("userId") String userId) {
 
 		List<Rating> ratings = Arrays.asList(new Rating(1000L, 4), new Rating(1001L, 5));
 
 		return ratings.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:7004/movies/" + rating.getMovieId(), Movie.class);
+			// Movie movie = restTemplate.getForObject("http://localhost:7004/movies/" +
+			// rating.getMovieId(), Movie.class);
+
+			Movie movie = webClientBuilder.build().get().uri("http://localhost:7004/movies/" + rating.getMovieId())
+					.retrieve().bodyToMono(Movie.class).block();
+
 			return new CatelogItem(movie.getName(), movie.getDesc(), rating.getRating());
 		}).collect(Collectors.toList());
 	}
